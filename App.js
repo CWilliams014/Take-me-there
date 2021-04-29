5f9522ef2519c3161b31f098f5977f9778e45fc9
Cole Williams <williamsch@mac.com>
2021-04-29 13:43:59 -0700

    lint config

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from "./src/screens/Login";
-import { useAuthState } from "react-firebase-hooks/auth";
-import Firebase from "./Firebase";
-
+import Login from './src/screens/Login';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Firebase from './Firebase';
 
 export default function App() {
-
-    const [user, loading, error] = useAuthState(Firebase.auth());
-    console.log('user', user)
-    console.log('loading :', loading)
-    console.log('error :', error  )
+  // const [user, loading, error] = useAuthState(Firebase.auth());
+  // console.log('user', user)
+  // console.log('loading :', loading)
+  // console.log('error :', error  )
   return (
     <View style={styles.container}>
       <Login />
